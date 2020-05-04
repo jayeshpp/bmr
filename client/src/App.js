bbd1ec209f0a42e427c47fd7c1cb1d62e4e3ae9b
jayeshpp <jayeshpp007@gmail.com>
2020-05-04 19:27:29 +0530

    client: initial project setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import './App.css';
-
-import axios from 'axios';
-import Hooks from './hooks'
-
 
 class App extends React.Component {
-  
-  state = {
-    hello: null
-  }
-
-  componentDidMount() {
-    axios.get('/hello')
-    .then(res => this.setState({hello: res.data}))
-    .catch(error => console.log(error))
-  }
 
   render() {
     
     return (
       <div>
-        {this.state.hello ? <div>{this.state.hello}</div> : null}
-        <Hooks/>
+        hello world.
       </div>
     );
   }
